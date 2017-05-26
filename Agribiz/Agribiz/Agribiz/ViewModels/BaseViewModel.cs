db19f612265ba72c3524176a9e11a0070dcec7f7
James Fisher <fisher.james@outlook.com>
2017-05-25 22:56:14 -0700

    Learn things from a default MasterDetail Xamarin project

---
+++ Agribiz/Agribiz/Agribiz/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Agribiz.Helpers;
+using Agribiz.Models;
+using Agribiz.Services;
+
+using Xamarin.Forms;
+
+namespace Agribiz.ViewModels
+{
+	public class BaseViewModel : ObservableObject
+	{
+		/// <summary>
+		/// Get the azure service instance
+		/// </summary>
+		public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+		bool isBusy = false;
+		public bool IsBusy
+		{
+			get { return isBusy; }
+			set { SetProperty(ref isBusy, value); }
+		}
+		/// <summary>
+		/// Private backing field to hold the title
+		/// </summary>
+		string title = string.Empty;
+		/// <summary>
+		/// Public property to set and get the title of the item
+		/// </summary>
+		public string Title
+		{
+			get { return title; }
+			set { SetProperty(ref title, value); }
+		}
+	}
+}
+
