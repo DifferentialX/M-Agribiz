d28a966f8edaad071b6737a9c6c15943f21da5fb
Aurel Lazar <lazar.aurel@gmail.com>
2017-07-06 21:26:14 -0700

    Add an image to the Product Details View Page

--- Agribiz/Agribiz/Agribiz/ViewModels/ProductsDetailViewModel.cs
+++ Agribiz/Agribiz/Agribiz/ViewModels/ProductsDetailViewModel.cs
@@ -10,10 +10,12 @@ namespace Agribiz.ViewModels
 	public class ProductsDetailViewModel : BaseViewModel<Product>
 	{
 		public Product Product { get; set; }
+		public string ImageUri { get; set; }
 		public ProductsDetailViewModel(Product product)
 		{
 			Product = product;
 			Title = Product.Title;
+			ImageUri = "http://m-agribiz.co/" + Product.Image;
 		}
 	}
 }
