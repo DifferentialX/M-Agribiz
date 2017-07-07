630a5d6ef08a01fc4b8a5248ee41eee1b5af6e64
Aurel Lazar <lazar.aurel@gmail.com>
2017-07-06 21:53:55 -0700

    Create custom view to make things look better

---
+++ Agribiz/Agribiz/Agribiz/Views/TextDetail.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Agribiz.Views
+{
+	public partial class TextDetail : ContentView
+	{
+		public static readonly BindableProperty DescriptorProperty = BindableProperty.Create(nameof(Descriptor), typeof(string), typeof(TextDetail));
+
+		public string Descriptor
+		{
+			get
+			{
+				return (string)GetValue(DescriptorProperty);
+			}
+			set
+			{
+				SetValue(DescriptorProperty, value);
+			}
+		}
+
+		public static readonly BindableProperty DetailProperty = BindableProperty.Create(nameof(Detail), typeof(string), typeof(TextDetail));
+
+		public string Detail
+		{
+			get
+			{
+				return (string)GetValue(DetailProperty);
+			}
+			set
+			{
+				SetValue(DetailProperty, value);
+			}
+		}
+
+		public TextDetail()
+		{
+			BindingContext = this;
+			InitializeComponent();
+		}
+	}
+}
